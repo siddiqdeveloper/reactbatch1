4fb3fc1b95061aa2de0fab470cdf99b93860324b
siddiq <asiddiq538@gmail.com>
2022-10-07 07:53:34 +0530

    new files

--- src/pages/AddComponent.js
+++ src/pages/AddComponent.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
+
 function AddComponent(){
-   return (<>add component</>);
+   const [form,setForm] = useState({courseName:'',description:''})
+
+   const handleSubmit = (e) =>{
+      e.preventDefault();
+      console.log(form);
+   }
+
+   return (<>
+      <div className="container">
+         <div className="row">
+               <div className="col-md-5 col-offset-3" style={{marginLeft:'24%'}}>
+                  <br/>
+               <form onSubmit={handleSubmit}>
+  <div class="form-group">
+    <label for="courseName">Course Name</label>
+    <input type="text" onKeyDown={ (e)=>{
+         setForm({...form,courseName:e.target.value});
+
+    }} class="form-control" id="courseName" aria-describedby="emailHelp" placeholder="Course Name"/>
+    <br/>
+  </div>
+  <div class="form-group">
+    <label for="courseDesc">Course Description</label>
+    <input type="text"  onKeyDown={ (e)=>{
+         setForm({...form,description:e.target.value});
+
+    }} class="form-control" id="courseDesc" placeholder="Course Description"/>
+  </div>
+  <br/>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+                     </div>
+         </div>
+      </div>
+   </>);
 }
 
 export default AddComponent;
