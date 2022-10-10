ba10b8414717f56e110fdc120cacf91479ab0ce8
siddiq <asiddiq538@gmail.com>
2022-10-10 07:38:39 +0530

    new files

--- src/pages/ListComponent.js
+++ src/pages/ListComponent.js
@@ -1,5 +1,35 @@
 function ListComponent(){
-   return <>list com</>
+   return <>
+   
+   
+<div class="container">
+  <h2>Course List</h2>
+  <p>The .table-bordered class adds borders to a table:</p>            
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Course Name</th>
+        <th>Description</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td> 
+           <button className="btn btn-warning btn-sm">Delete</button>
+         &nbsp;
+           <button className="btn btn-primary btn-sm">View</button>
+         </td>
+      </tr>
+     
+    </tbody>
+  </table>
+</div>
+
+   
+   </>
 }
 
 export default ListComponent;
